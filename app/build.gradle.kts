4bacaea19ee1692fde3aa67b1da9125167ccb914
AILIFE <ailife8881@gmail.com>
2026-06-24 20:15:29 +0800

    Bump version to M2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
 }
 
 apkMixin {
-    versionName = "M1.5"
+    versionName = "M2.0"
     targetApk = "source.apk"
     useProcessorCountAsThreadCount = project.properties["useProcessorCountAsThreadCount"] == "true"
 
